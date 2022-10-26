622ad29ef4bab449255f131c1f3d089153c9e21b
Nadia Caroline <108709281+NadiaCaroline@users.noreply.github.com>
2022-10-26 18:05:07 +0700

    commit

---
+++ src/views/Dashboard/ToDo.vue
@@ -0,0 +1,24 @@
+<script>
+import { mapActions, mapState } from "pinia";
+import d$todo from "@/stores/dashboard/todo";
+
+export default {
+  name: "ToDo",
+  computed: {
+    ...mapState(d$todo, ["g$list"]),
+  },
+  methods: {
+    ...mapActions(d$todo, ["a$list"]),
+    async getlist() {
+      try {
+        await this.a$list();
+      } catch (e) {
+        console.error("methods getlist error", e);
+      }
+    },
+  },
+  async created() {
+    await this.getlist();
+  },
+};
+</script>
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: "/kelompok-6/",
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-})
+});
